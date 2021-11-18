64d1fb37c632513f7e9709073580610238b4da92
witekmikolajczak <witekmikolajczak@gmail.com>
2021-11-18 18:43:50 +0100

    Added 'Card disable' property to component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
 import { Checkbox } from "../Checkbox/Checkbox.jsx";
 import styles from "./Card.module.scss";
 
 const Card = (props) => {
   const { isCheckboxVisible } = props;
+  const { isCardDisabled } = props;
   return (
-    <div className={styles.card}>
+    <div
+      className={
+        // Setting Card overlay effect:
+        isCardDisabled
+          ? `${classNames(styles.overlay, styles.card)}`
+          : styles.card
+      }
+    >
       <div className={styles.wrapper}>
         <div className={styles.text}>
           <h3>Primary text</h3>
           <p>Secondary text</p>
         </div>
+
+        {/* Setting Checkbox visibility: */}
         {isCheckboxVisible && (
           <div className={styles.checkbox}>
             <Checkbox />
@@ -25,5 +36,6 @@ const Card = (props) => {
 
 Card.propTypes = {
   isCheckboxVisible: PropTypes.bool,
+  isCardDisabled: PropTypes.bool,
 };
 export default Card;
